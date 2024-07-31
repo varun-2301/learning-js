eb2c3e9461f979f38dcdc2d1512d19e790e3e521
Varun Kumar <kumarvarun2301@gmail.com>
2024-07-31 13:17:01 +0530

    js answers updated

---
+++ programs/bind-polyfill/index.js
@@ -0,0 +1,19 @@
+Function.prototype.customBind = function(...args){
+    let obj = this
+        params = args.slice(1)
+    return function(...args2){
+        obj.apply(args[0], [...params, ...args2])
+    }
+}
+
+let name = {
+    firstname : 'Varun',
+    lastname : 'Kumar'
+}
+
+let printName = function (state, country) {
+    console.log(this.firstname + " " + this.lastname + " " + state + " " + country)
+} 
+
+let output = printName.customBind(name, 'Delhi')
+output('India')
