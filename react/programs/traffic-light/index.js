5e793429ceef9c3371548173c56c27da2a519f25
Varun Kumar <varun.kumar@tavant.com>
2025-09-12 15:13:33 +0530

    added react program

---
+++ react/programs/traffic-light/index.js
@@ -0,0 +1,25 @@
+const App = () => {
+    const [light, setLight] = React.useState('red')
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => {
+            if(light === 'red') setLight('yellow')
+            if(light === 'yellow') setLight('green')
+            if(light === 'green') setLight('red')
+        }, light === 'red' ? 3000 : light === 'yellow' ? 1000 : 2000)
+    }, [light])
+
+    return (
+        <div>
+            <h2 data-testid="title">Traffic Lights</h2>
+            <div className="traffic-light" id="traffic-light" data-testid="traffic-light">
+                <div id="red-light" data-testid="red-light" className={`circle ${light === 'red' ? 'red-on' : ''}`}></div>
+                <div id="yellow-light" data-testid="yellow-light" className={`circle ${light === 'yellow' ? 'yellow-on' : ''}`}></div>
+                <div id="green-light" data-testid="green-light" className={`circle ${light === 'green' ? 'green-on' : ''}`}></div>
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
